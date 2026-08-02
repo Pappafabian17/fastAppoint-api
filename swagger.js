efa7f29d3acb679dbe0d3688714dcf775ce339f6
Fabian Pappa <fabian.pappa.ext@errepar.com>
2026-08-01 21:50:18 -0300

    correcting swagger.js

--- swagger.js
+++ swagger.js
@@ -1,14 +1,22 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+
+const host = process.env.RENDER_EXTERNAL_URL 
+  ? process.env.RENDER_EXTERNAL_URL.replace(/^https?:\/\//, '') 
+  : 'localhost:3000';
+
+const schemes = process.env.RENDER_EXTERNAL_URL ? ['https'] : ['http'];
+
 const doc = {
   info: {
     title: 'FastAppoint API',
     description: 'Backend API for FastAppoint - Booking management system',
-  }
+  },
+  host: host,
+  schemes: schemes 
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
-
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
